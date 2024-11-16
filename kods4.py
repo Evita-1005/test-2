06ac93d5b96a926d5debeb5b6daf82eaa961b4de
Evita-1005 <konudarisim@gmail.com>
2024-11-17 01:29:41 +0200

    Izvaidots jauns kods (4)

---
+++ kods4.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    
+    rnd = random.randint(1,9) 
+
+    x = int(input("Uzmini skaiktli 1 līdz 9: "))
+    count = 1
+
+    while (x != rnd):
+        count += 1
+        x = int(input("Neuzminēji, mēģini vēlraiz, ievadi skaiktli 1 līdz 9: "))
+    
+    print(f" Apsveicam, tu uzzmunēji skaitli {x} ar {count} reizi!")
+if __name__ == "__main__":
+    main() 
